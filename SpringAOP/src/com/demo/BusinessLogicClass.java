7a81bfde49ee622beaff304a2279248fa2c59068
rajeevshukla <mail2rajeevshukla@gmail.com>
2015-08-12 22:22:46 +0530

    spring aop basic concepts programs added.

---
+++ SpringAOP/src/com/demo/BusinessLogicClass.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class BusinessLogicClass {
+
+	public void doProcess(){
+		System.out.println("============ Content is being processed======= ");
+	}
+	
+	public void doProcess1() {
+		System.out.println("==== Do process 1 business login====");
+	}
+	
+	public void doProcess(int x) {
+		System.out.println("==== Do process with parameter.. business login====");
+	}
+	
+	
+}
