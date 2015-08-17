4e5360a94386f00bbfa103509fbbe7ba2bb13ad6
rajeevshukla <mail2rajeevshukla@gmail.com>
2015-08-17 22:15:35 +0530

    spring hello world added.

---
+++ SpringMVCHelloWorld/src/com/demo/MyController.java
@@ -0,0 +1,46 @@
+package com.demo;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController { //this bean will be registered as a handler bean.
+
+	@Autowired
+ private ProcessDataService processData;
+	
+	
+	@RequestMapping("/abc.htm")
+	public String abc(){
+		System.out.println("we caught your request ");
+		return "index"; //index is a view name
+	}
+	
+	
+	@RequestMapping("/welcome.htm")
+	public ModelAndView pqr(){
+		ModelAndView mav=new ModelAndView("admin/welcome");
+		processData.processData();
+		mav.addObject("userName", "rajeev");
+		return mav;
+	}
+	
+	@RequestMapping("/welcome1.htm")
+	public Model pqr1(Model model){
+		
+		
+		return model;
+	}
+	
+	@RequestMapping("/welcome2.htm")
+	public void pqr2(Model model,HttpSession session){
+		
+		
+	}
+	
+}
