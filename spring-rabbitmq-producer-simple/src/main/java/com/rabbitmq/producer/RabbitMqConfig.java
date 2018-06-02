3f21b72178f3b17983b446181781d190d9613bc5
rajeevshukla <mail2rajeevshukla@gmail.com>
2018-06-01 20:37:14 -0400

    adding docker compose.

---
+++ spring-rabbitmq-producer-simple/src/main/java/com/rabbitmq/producer/RabbitMqConfig.java
@@ -0,0 +1,21 @@
+package com.rabbitmq.producer;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+	
+	//create queue
+	
+	//create exchange
+	
+	//create binding.. 
+	
+	
+	// Create Listener...
+	
+	// create listern adaptor.. 
+	
+	
+}
